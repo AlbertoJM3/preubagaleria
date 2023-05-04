4c65eff9026a69e6c939a6a3e343a72a782e9bd0
AlbertoJM3 <129084031+AlbertoJM3@users.noreply.github.com>
2023-05-04 21:04:08 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,41 +1,77 @@
+name: CI/CD todo
 
-# From https://github.com/datademofun/heroku-basic-flask
-# Nota: en ese tutorial se ve como hacerlo 
-#  - con Heroku CLI
-#  - sin usar GitHub
-#   Aunque luego al final lo manda a GitHub
+on:
+  push:
+    branches: [ main ]
+    paths:
+      - src/**
+  pull_request:
+    branches: [ main ]
+    paths:
+      - src/**
 
-from flask import Flask, render_template
-from datetime import datetime
-import locale
+jobs:
 
+  flake8-lint:
+    name: Lint with flake8
+    runs-on: ubuntu-latest
+    steps:
+      - name: Check out source repository
+        uses: actions/checkout@v3
+      - name: Set up Python environment
+        uses: actions/setup-python@v4
+        with:
+          python-version: "3.11"
+      - name: flake8 Lint
+        uses: py-actions/flake8@v2
 
-app = Flask(__name__)
-locale.setlocale(locale.LC_TIME, '')
+  build:
+    name: build and push Docker image
+    needs: flake8-lint
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v3
+          
+      - name: Login to DockerHub
+        uses: docker/login-action@v2 
+        with:
+          username: ${{ secrets.DOCKERHUB_USERNAME }}
+          password: ${{ secrets.DOCKERHUB_TOKEN }}
+          
+      - name: Build and push Docker image
+        uses: docker/build-push-action@v4
+        with:
+          context: .
+          push: true 
+          tags: ${{ secrets.DOCKERHUB_USERNAME }}/galeria:latest
+      # - name: Sleep for 60 seconds
+      #   run: sleep 60s
+      #   shell: bash
 
-@app.route('/')
-def homepage():
-  
-    the_time = datetime.now().strftime("%A, %d %b %Y %H:%M")
-
-    # Render HTML with variable
-    return render_template("index.html", the_time=the_time, tema="cats" )
-
-    # return f"""
-    # <h1>Hello heroku</h1>
-    # <p>It is currently {the_time}.</p>
-    # <img src="http://loremflickr.com/600/400" />
-    # """
-
-
-@app.route('/spain')
-def homepage_spain():
-
-    the_time = datetime.now().strftime("%A, %d %b %Y %H:%M")
-
-    # Render HTML with variable
-    return render_template("index.html", the_time=the_time, tema="spain" )
-
-
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True) 
+  aws:
+    name: Deploy image to aws
+    needs: build
+    runs-on: ubuntu-latest
+    steps:
+    - name: multiple command
+      uses: appleboy/ssh-action@master
+      with:
+        host: ${{ secrets.HOSTNAME }}
+        username: ${{ secrets.USER_NAME }}
+        key: ${{ secrets.AWS_PRIVATE_KEY }}
+        port: 22  # ${{ secrets.PORT }}
+        script: |
+            sleep 30
+            docker stop web && docker rm web
+            docker rmi angoies/galeria
+            docker run -d -p 80:5000 --name web angoies/galeria
+            sleep 30
+  curl:
+    name: Test URL con curl
+    needs: aws
+    runs-on: ubuntu-latest
+    steps:
+    - name: curl
+      uses: wei/curl@master
+      with:
+        args: http://${{ secrets.HOSTNAME }}
